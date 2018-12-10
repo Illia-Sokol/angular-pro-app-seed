7bf7f3d3a817ec7be830d022106dadfa3a843ad6
Illia-Sokol <illiasokolovskyi@gmail.com>
2018-12-11 00:42:54 +0200

    update cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-import { Store } from 'store';
-
-// feature modules
-
-// containers
-import { AppComponent } from './containers/app/app.component';
-
-// components
-
-// routes
-export const ROUTES: Routes = [];
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(ROUTES)
-  ],
   declarations: [
     AppComponent
   ],
-  providers: [
-    Store
+  imports: [
+    BrowserModule,
+    AppRoutingModule
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
-
-// var config = {
-//   apiKey: "AIzaSyCUBiRVUDe4U6yXVSxKVSY7DxMKMUtBEkI",
-//   authDomain: "fitness-app-17c3d.firebaseapp.com",
-//   databaseURL: "https://fitness-app-17c3d.firebaseio.com",
-//   projectId: "fitness-app-17c3d",]
-//   storageBucket: "fitness-app-17c3d.appspot.com",
-//   messagingSenderId: "761197483639"
-// };
+export class AppModule { }
